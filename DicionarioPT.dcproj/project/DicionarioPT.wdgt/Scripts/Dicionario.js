5a9c0cee116ccd97a790e46160a4e82ead899173
rcg <rcgoncalves.pt@gmail.com>
2013-09-29 18:55:26 +0100

    Update parser and output layout
    Fix bugs resulting from Priberam website changes.

--- DicionarioPT.dcproj/project/DicionarioPT.wdgt/Scripts/Dicionario.js
+++ DicionarioPT.dcproj/project/DicionarioPT.wdgt/Scripts/Dicionario.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006, José Coelho (jose.alberto.coelho@gmail.com)
- * Copyright (C) 2012, Rui Carlos Gonçalves (rcgoncalves.pt@gmail.com)
+ * Copyright (C) 2013, Rui Carlos Gonçalves (rcgoncalves.pt@gmail.com)
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation; either version 2 of the License,
  * or (at your option) any later version.
@@ -19,86 +19,57 @@ var scrollArea;
 var scrollBar;
 
 function LoadWidget() {
-	Web = new URL();
-	Widget.setSize();
-	Widget.progressLoad("progressGraphic");
-	scrollBar = new AppleVerticalScrollbar( document.getElementById('scrollBar') );
-	scrollArea = new AppleScrollArea( document.getElementById('scrollArea'), scrollBar );
-	scrollArea.addScrollbar( scrollBar );
-	scrollBar.show();
-	Widget.shrink();
+  Web = new URL();
+  Widget.setSize();
+  Widget.progressLoad("progressGraphic");
+  scrollBar = new AppleVerticalScrollbar(document.getElementById('scrollBar'));
+  scrollArea = new AppleScrollArea(document.getElementById('scrollArea'), scrollBar);
+  scrollArea.addScrollbar(scrollBar);
+  scrollBar.show();
+  Widget.shrink();
 }
 
 function shrinkExpand(event) {
-    if(Widget.close) {
-        Widget.expand();
-    }
-    else {
-        Widget.shrink();
-    }
+  if(Widget.close) {
+    Widget.expand();
+  }
+  else {
+    Widget.shrink();
+  }
 }
 
+/**
+ * Searches for a word definition.
+ */
 function Search(value) {
-	/* needed because may exist links on contents */
-	document.getElementById("SearchField").value = value;
-	if( value.length == 0 ) {
-		/* nothing on search input, close widget */
-		Widget.shrink();
-		document.getElementById("contents").innerHTML = "";
-		
-	}
-	else {
-		/* visual output */
-		Widget.progressStart();
-		Web.location = "http://www.priberam.pt/dlpo/default.aspx?pal="+value;
-		Web.fetchAsync( GetResult );
-	}
+  document.getElementById("SearchField").value = value;
+  if(value.length == 0) {
+    Widget.shrink();
+    document.getElementById("contents").innerHTML = '';
+  }
+  else {
+    Widget.progressStart();
+    Web.location = "http://www.priberam.pt/dlpo/" + value;
+    Web.fetchAsync(GetDefinition);
+  }
 }
 
-function GetResult(webfetch) {
-    document.getElementById("contents").innerHTML = webfetch.result;
-    var htmlDef = document.getElementById("DivDefinicao");
-    document.getElementById("contents").innerHTML = "";
-    var def = getDefinition(htmlDef);
-	if(def) {
-		document.getElementById("contents").innerHTML = def;
-	}
-	else {
-		document.getElementById("contents").innerHTML = getSuggestions(htmlDef);
-	}
-	
-	if(Widget.needResize()) {
-		Widget.expand();
-	}
-	Widget.progressStop();
-	scrollArea.refresh();
-    scrollBar.refresh();
-    scrollArea.verticalScrollTo(0);
-}
-
-function Conjugation(value) {
-	/* needed because may exist links on contents */
-	document.getElementById("SearchField").value = value;
-	
-	if( value.length == 0 ) {
-		/* nothing on search input, close widget */
-		Widget.shrink();
-		document.getElementById("contents").innerHTML = "";
-	}
-	else {
-		/* visual output */
-		Widget.progressStart();
-		Web.location = "http://www.priberam.pt/dlpo/Conjugar.aspx?pal="+value;
-		Web.fetchAsync( GetConjugation );
-	}
-}
-
-function GetConjugation(webfetch)
-{
-  document.getElementById("contents").innerHTML = getConjugation(webfetch.result);
-  	
-  if( Widget.needResize() ) {
-	Widget.expand();
+/**
+ * Callback function that processes a word definition.
+ */
+function GetDefinition(webfetch) {
+  document.getElementById('contents').innerHTML = webfetch.result;
+  var content = getMainContent();
+  document.getElementById('contents').innerHTML = '';
+  var def = getDefinition(content);
+  if(def) {
+    document.getElementById('contents').innerHTML = def;
+  }
+  else {
+    document.getElementById('contents').innerHTML = getSuggestions(content);
+  }
+  if(Widget.needResize()) {
+    Widget.expand();
   }
   Widget.progressStop();
   scrollArea.refresh();
@@ -106,24 +77,60 @@ function GetConjugation(webfetch)
   scrollArea.verticalScrollTo(0);
 }
 
+/**
+ * Obtains a verb conjugation.
+ */
+function Conjugation(value) {
+  document.getElementById('SearchField').value = value;
+  if( value.length == 0 ) {
+    Widget.shrink();
+    document.getElementById('contents').innerHTML = '';
+  }
+  else {
+    Widget.progressStart();
+    Web.location = 'http://www.priberam.pt/dlpo/Conjuga/' + value;
+    Web.fetchAsync( GetConjugation );
+  }
+}
+
+/**
+ * Callback function that processes a verb conjugation.
+ */
+function GetConjugation(webfetch) {
+
+  document.getElementById('contents').innerHTML = webfetch.result;
+  var content = getMainContent(webfetch.result);
+  document.getElementById('contents').innerHTML = '';
+  document.getElementById('contents').innerHTML = getConjugation(content);
+  if(Widget.needResize()) {
+    Widget.expand();
+  }
+  Widget.progressStop();
+  scrollArea.refresh();
+  scrollBar.refresh();
+  scrollArea.verticalScrollTo(0);
+}
+
+/**
+ * Opens widget website.
+ */
 function openWebsite()  {
-    widget.openURL('http://rcgoncalves.pt/project/dicionariopt/');
+  widget.openURL('http://rcgoncalves.pt/project/dicionariopt/');
 }
 
 /*
- * Remove html tags or make them a "clean" version
+ * Removes html tags or make them a "clean" version
  * first argument - the string to clean
  * second argument - bool - dummy tag or remove
  * other arguments - html tags to clean/remove
  */
-function stripTags()
-{
-	var string = arguments[0];
-	var strip = arguments[1];
-	for(var i = 2; i < arguments.length; i++)
-	{
-		var regex = new RegExp("<"+arguments[i]+"[^>]*>","img");
-		string = string.replace(regex, strip ? "" : "<"+arguments[i]+">");
-	}
-	return string;
+function stripTags() {
+  var string = arguments[0];
+  var strip = arguments[1];
+  for(var i = 2; i < arguments.length; i++)
+  {
+    var regex = new RegExp('<' + arguments[i] + '[^>]*>', 'img');
+    string = string.replace(regex, strip ? '' : '<' + arguments[i] + '>');
+  }
+  return string;
 }
--- DicionarioPT.dcproj/project/DicionarioPT.wdgt/Scripts/Parser.js
+++ DicionarioPT.dcproj/project/DicionarioPT.wdgt/Scripts/Parser.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012, Rui Carlos Gonçalves (rcgoncalves.pt@gmail.com)
+ * Copyright (C) 2013, Rui Carlos Gonçalves (rcgoncalves.pt@gmail.com)
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation; either version 2 of the License,
  * or (at your option) any later version.
@@ -13,127 +13,123 @@
  */
 
 // ======================================================================================================
+// INITIAL PARSING ======================================================================================
+// ======================================================================================================
+
+/**
+ * Finds the block that contains the relevant content.
+ */
+function getMainContent() {
+  var divs = document.getElementsByTagName('div');
+  var div = null;
+  var i = divs.length - 1;
+  var stop = false;
+  while(i >= 0 && !stop) {
+    div = divs[i];
+    if(div.className != null && div.className.indexOf('pb-main-content') > -1) {
+      stop = true;
+    }
+    i--;
+  }
+  return div;
+}
+
+// ======================================================================================================
 // WORD DEFINITION ======================================================================================
 // ======================================================================================================
 
 /**
- * Get word definition
+ * Returns a word definition.
  */
 function getDefinition(html)
 {
-  if(html == null) return null;
-  else if(html.innerHTML.match("FormataSugestoesENaoEncontrados")) return null;
-  else {
-    html = getDefinitionStructure(html);
-    html = parseDefinitionStructure(html);
-    definition = html.innerHTML;
-    definition = stripTags(definition,true,"font","img","Definicao","/Definicao");
-    return definition;
+  var definition = null;
+  if(html != null) {
+    if(html.innerHTML.indexOf('alert-info') < 0 ||
+        (html.innerHTML.indexOf('Palavra não encontrada') < 0
+	&& html.innerHTML.indexOf('Palavra reconhecida pelo FLiP mas sem definição no dicionário') < 0)
+	) {
+      html = html.getElementsByTagName('div')[0];
+      html = parseDefinitionStructure(html);
+      if(html != null) {
+        definition = html.innerHTML;
+	definition = stripTags(definition, true,
+	    'font',
+	    'img',
+	    'notas', '/notas',
+	    'categoria_ext_aao', '/categoria_ext_aao',
+	    'pt', '/pt',
+	    'aao', '/aao',
+	    'dominio_ext_aao', '/dominio_ext_aao');
+      }
+    }
   }
+  return definition;
 }
 
 /**
- * Finds the block that contains the definition.
- */
-function getDefinitionStructure(html) {
-  var i = html.children.length - 1;
-  for( ; i >= 0; i--) {
-    var elem = html.children[i];
-    if(elem.tagName == 'DEF') {
-      return elem;
-    }
-    else {
-      rec = getDefinitionStructure(elem);
-      if(rec) return rec;
-    }
-  }
-  return null;
-}
-
-/**
- * Parse definition.
- * Adjust URLs, remove some attributes, remove some tags, etc.
+ * Parses a word definition.
+ * Adjust URLs, removes some attributes, remove some tags, etc.
  */
 function parseDefinitionStructure(html) {
-  var i = html.children.length;
-  for( ; i > 0; i--) {
+  for(var i = html.children.length ; i > 0; i--) {
     var elem = html.children[i-1];
     var toDel = 0;
     switch(elem.tagName) {
-      case "A" :
-        var url=elem.getAttribute("href");
+      case 'A' :
+        var url=elem.getAttribute('href');
         if(url!=null) {
-          if((!url.match(/(.|\n)*Conjugar.aspx\?pal=([^\"]+)/ig)) //"
-              && (!url.match(/(.|\n)*default.aspx\?pal=([^\"]+)/ig)) //"
-              && (!url.match(/dicionario@priberam.pt/))) {
-            elem.removeAttribute("href");
+          if(!url.match(/\/dlpo\//ig)) {
+            elem.removeAttribute('href');
           }
           else {
-            url=url.replace(/(.|\n)*Conjugar.aspx\?pal=([^\"]+)/ig,"javascript:Conjugation('$2')"); //"
-            url=url.replace(/(.|\n)*default.aspx\?pal=([^\"]+)/ig,"javascript:Search('$2')"); //"
-            elem.setAttribute("href", url);
-            elem.innerHTML=elem.innerHTML.replace(/^\s*/,"");
-            elem.innerHTML=elem.innerHTML.replace(/\s*$/,"");
+            url=url.replace(/(.|\n)*\/dlpo\/Conjuga\/(.+)/ig, "javascript:Conjugation('$2')");
+	    url=url.replace(/(.|\n)*\/dlpo\/(.+)/ig, "javascript:Search('$2')");
+            elem.setAttribute('href', url);
           }
         }
         break;
-      case "REFS_EXTERNAS" :
-      case "SCRIPT" :
+      case 'REFS_EXTERNAS' :
+      case 'SCRIPT' :
         toDel = 1;
         break;
-      case "DIV" :
-        var divId=elem.getAttribute('id');
-        if(divId=='ctl00_ContentPlaceHolder1_pnl_ultimas_pesquisas'
-            || divId=='ctl00_ContentPlaceHolder1_pnl_nuvem_palavras'
-            || divId=='ctl00_ContentPlaceHolder1_pnl_VerVideo'
-            || divId=='ctl00_ContentPlaceHolder1_pnl_Traduzir'
-            || divId=='ctl00_ContentPlaceHolder1_pnl_relacionadas') {
-          toDel = 1;
-        }
-        else {
-          var style=elem.getAttribute("style");
-          if(style != null) {
-            if(style.match(/background-color:\s*#f1f1f1;/ig)) toDel = 1;
-            else {
-              style=style.replace(/background-color:\s*#eee;/,"");
-              elem.setAttribute("style", style);
-            }
-          }
-        }
+      case 'DIV' :
+        var style = elem.style;
+	if(style.display == 'none') toDel = 1;
         break;
-      case "TD" :
-      case "TABLE" :
+      case 'TD' :
+      case 'TABLE' :
         removeAttributes(elem);
         break;
-      case "SPAN" :
-        if(elem.getAttribute("class") == "varpb") toDel = 1;
-        else {
-          elem.removeAttribute("ondblclick");
-          elem.removeAttribute("title");
-        }
+      case 'SPAN' :
+        if(elem.className == 'varpb') toDel = 1;
         break;
-      case "BR" :
+      case 'BR' :
         toDel = 2;
+	break;
+      case 'CATEGORIA_EXT_AAO' :
+        var newElem = document.createElement('SPAN');
+	newElem.className = 'dpt-categoria';
+	newElem.innerHTML = elem.innerHTML;
+	elem.innerHTML = '';
+	elem.appendChild(newElem);
+        break;
     }
     if(toDel == 1) {
       html.removeChild(elem);
-      if(i > 1 && html.children[i-2].tagName == "BR") {
+      if(i > 1 && html.children[i-2].tagName == 'BR') {
         html.removeChild(html.children[i-2]);
         i--;
       }
     }
-    else if(toDel == 2 && i > 1 && html.children[i-2].tagName == "BR") {
+    else if(toDel == 2 && i > 1 && html.children[i-2].tagName == 'BR') {
       html.removeChild(elem);
     }
     else if(elem.innerHTML.match(/^\s*$/) && toDel != 2) {
       html.removeChild(elem);
     }
     else {
-      var style=elem.getAttribute("style");
-      if(style != null) {
-        style=style.replace(/color:#999;/g,"color:#222;");
-        elem.setAttribute("style", style);
-      }
+      elem.removeAttribute('style');
       // recursive call
       parseDefinitionStructure(elem);
     }
@@ -146,34 +142,41 @@ function parseDefinitionStructure(html) {
 // ======================================================================================================
 
 /**
- * Get suggestions
+ * Returns a feedback message for words that do not have a definition available.
  */
-function getSuggestions(htmlDef) {
+function getSuggestions(html) {
   var result = "";
-  var list = getSuggestionsList(htmlDef);
-  for(i in list) {
-    result += "<li><a href=\"javascript:Search('" + list[i] + "')\">" + list[i] + "</a></li>\n";
+  if(html.innerHTML.indexOf('Palavra reconhecida pelo FLiP mas sem definição no dicionário') > -1) {
+    result = '<p>Palavra reconhecida mas sem definição no disponível.</p>';
   }
-  if(result != "") {
-    result = "Existem as seguintes sugest&otilde;es:\n" + "<ul style=\"margin: 3px; padding-left: 18px; text-indent: -3px; list-style-type: square;\">\n" + result + "</ul>\n";
-    return result;
+  else {
+    var list = getSuggestionsList(html);
+    for(i in list) {
+      result += '<li><a href="javascript:Search(\'' + list[i] + '\')">' + list[i] + '</a></li>\n';
+    }
+    if(result != '') {
+      result = '<p style="font-weight:bold;">Palavra não encontrada.</strong></p><p>Experimente uma das seguintes sugestões:</p>\n'
+          + '<ul style="margin: 3px; padding-left: 18px; text-indent: -3px; list-style-type: square;">\n'
+	  + result
+	    + '</ul>\n';
+    }
+    else {
+      result = '<p style="font-weight:bold;">Palavra não encontrada.</p><p>Não existem sugestões.</p>';
+    }
   }
-  else return "N&atilde;o existem sugest&otilde;es.";
+  return result;
 }
 
 /**
- * Get list of suggestions.
+ * Returns list of suggestions.
  */
-function getSuggestionsList(htmlDef) {
+function getSuggestionsList(html) {
   var result = new Array();
-  for(i = 0; i < htmlDef.children.length; i++) {
-    var elem = htmlDef.children[i];
-    var id = elem.getAttribute("id");
-    if(elem.tagName == "DIV" && id != null && id == "FormataSugestoesENaoEncontrados") {
-      result.push(elem.innerText);
-    }
-    else if(elem.innerHTML.match(/FormataSugestoesENaoEncontrados/)) {
-      result = result.concat(getSuggestionsList(elem));
+  var divs = html.getElementsByTagName('div');
+  for(var i = 0; i < divs.length; i++) {
+    var div = divs[i];
+    if(div.id == 'FormataSugestoesENaoEncontrados') {
+      result.push(div.innerText);
     }
   }
   return result;
@@ -184,92 +187,37 @@ function getSuggestionsList(htmlDef) {
 // ======================================================================================================
 
 /**
- * Get verb conjugation
+ * Returns a verb conjugation.
  */
 function getConjugation(html) {
-  var str = getFirstElementByTagName(html,"body");
-  str = getFirstElementByTagName(str,"body",10);
-  
-  str = stripTags(str, true, "body", "font", "/body");
-  str = str.replace(/background-color:\s*#eee;/g,"");
-  str = str.replace(/(<br>)*$/g,"");
-  
-  return str;
+  var result = html.children[0];
+  // remove meta tags
+  var elems = result.getElementsByTagName('meta');
+  for(var i = elems.length - 1; i >= 0; i--) {
+    var elem = elems[i];
+    elem.parentNode.removeChild(elem);
+  }
+  // remove style tags
+  elems = result.getElementsByTagName('style');
+  for(var i = elems.length - 1; i >= 0; i--) {
+    var elem = elems[i];
+    elem.parentNode.removeChild(elem);
+  }
+  // remove span tags
+  elems = result.getElementsByTagName('span');
+  for(var i = elems.length - 1; i >= 0; i--) {
+    var elem = elems[i];
+    if(elem.style.color == 'rgb(255, 255, 255)') {
+      elem.parentNode.removeChild(elem);
+    }
+  }
+  var regex = new RegExp('/ elas?/ vocês?', 'g');
+  return result.innerHTML.replace(regex, '');
 }
 
 // ======================================================================================================
 // AUXILIAR FUNCTIONS ===================================================================================
 // ======================================================================================================
-
-/**
- * Returns the first element from XML formatted string 'str', with tag name 'tag'.
- * It starts searching from 'start'.
- */
-function getFirstElementByTagName(str, tag, start) {
-  var strx = str;
-  if(start) strx = strx.substr(start);
-  strx = removeComments(strx);
-  var res = "";
-  var open = new RegExp("<"+tag+"[^>]*>","i");
-  var close = new RegExp("</"+tag+" *>","i");
-  var pos1 = strx.search(open);
-  var pos2;
-  var pos = pos1;
-  var count;
-  
-  if(pos < 0) return "";
-  else {
-    strx = strx.substr(pos);
-    res = strx.substr(0,1);
-    strx = strx.substr(1);
-    count = 1;
-    var error = false;
-    
-    while(count > 0 && !error) {
-      pos1 = strx.search(open);
-      pos2 = strx.search(close);
-      if(pos2 < 0) error = true;
-      
-      if(pos1 < pos2 && pos1 >= 0) { //open tag
-        count++;
-        pos = pos1;
-      }
-      else if(pos2 >= 0) { //close tag
-        count--;
-        pos = pos2;
-      }
-      
-      res += strx.substr(0,pos + 1);
-      strx = strx.substr(pos + 1);
-    }
-    
-    if(error) return "";
-    else return res + "/" + tag + ">";
-  }
-}
-
-/**
- * Remove XML comments from string 'str'.
- */
-function removeComments(str) {
-  return str.replace(/<!--[\s\S]*?-->/g);
-}
-
-/**
- * Remove consecutive spaces from string 'str'.
- */
-function removeSpaces(str) {
-  var res=str.replace(/\s+/g,"");
-  return res;
-}
-
-/**
- * Removes all tags from string 'str'.
- */
-function removeTags(str) {
-  var res=str.replace(/<[^>]*>/g,"");
-  return res;
-}
 
 /**
  * Remove the attributes of a HTMLElement.
